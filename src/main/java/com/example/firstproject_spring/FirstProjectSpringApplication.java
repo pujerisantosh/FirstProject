0ccf558b8805871b299cc3e6be56a308cb90a38c
Santosh Pujeri <16santoshpujari@gmail.com>
2024-12-10 15:42:36 +0530

    FirstProject

---
+++ src/main/java/com/example/firstproject_spring/FirstProjectSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstproject_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstProjectSpringApplication.class, args);
+    }
+
+}
