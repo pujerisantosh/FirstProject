3eea480da149e747464e43c8ef26343ee818f34f
Santosh Pujeri <16santoshpujari@gmail.com>
2024-12-10 15:48:43 +0530

    FirstProject

--- src/main/java/com/example/firstproject_spring/FirstProject.java
+++ src/main/java/com/example/firstproject_spring/FirstProject.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FirstProject {
 
-    @RequestMapping("/Hello")
+    @RequestMapping("/hello")
         public String sayHello(){
 
-        return "Hello";
+        return "hello";
         }
     }
 
